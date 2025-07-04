213d3dca20a889a332077c7ef1243d7372892a91
BINEESHA KP <bineeshaharilal@gmail.com>
2025-07-04 13:30:30 +0530

    Add files via upload

--- code/chatbot.py
+++ code/chatbot.py
@@ -1,1 +1,24 @@
+# Task-8
+# Rule-based Chatbot
+# Author: Bineesha K P
+# Date: 25-06-2025
 
+def chatbot():
+    print("Hy! I am Bitu the Chatbot!. (Type 'exit' to end the chat)")
+    
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input in ["hi", "hello"]:
+            print("Bitu: Hello there! How can I help you?")
+        elif user_input in ["how are you?", "how are you"]:
+            print("Bitu: I'm doing great.Thank You!")
+        elif user_input in ["what is your name?", "your name"]:
+            print("Bitu: I am Bitu, your friendly chatbot.")
+        elif user_input in ["bye", "exit"]:
+            print("Bitu: Goodbye! Have a nice day.")
+            break
+        else:
+            print("Bitu: Sorry, I didn't understand that.")
+
+chatbot()
